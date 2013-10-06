bc0f75cdb1d178db0f9c2f281c3bbe6e2658fcae
root <root@mail.clixster.net>
2013-10-06 17:19:43 +0700

    update

--- README.md
+++
@@ -1,4 +0,0 @@
-pulsa
-=====
-
-pulsa
--- index.php
+++ index.php
@@ -1,3 +1,48 @@
-<?
-echo "saru";
-?>
+<HTML>
+<HEAD>
+<TITLE>Client ISP INTERSAT</TITLE>
+</HEAD>
+<table width="150%" border="0">
+  <tr><!-- Row 1 -->
+      <td width=500 nowarp="">
+      <table border=1 bordercolordark=#ffffe0 bordercolorlight=#000000 cellpadding=2 cellspacing=0 bgcolor='#ffffe0' valign="top">
+
+<form method=post action=search.php>
+<tr>
+<td>Nama Reseller</td><td>
+                <select name=ecolink>
+		                <option value="" selected>--Nama Reseller---
+				<?
+				include "config.php";
+				$conn = mysql_connect($host, $user, $pass);
+				mysql_select_db("kalkun", $conn);
+				$sqlz = "SELECT * FROM reseller order by id";
+				$conn = mysql_connect($host, $user, $pass);
+				$resultz = mysql_query($sqlz, $conn);
+				while ($row = mysql_fetch_array($resultz)){
+				
+				$nama = $row["nama"];
+				
+				$telepon = $row["phone"];
+				
+				echo "<option value=$telepon>$nama\n";
+				
+				}
+				?>
+				</option>
+				</select>
+				
+				</td>
+				                <tr>
+						
+<td><input type=submit name=changepasswd value="Search User">
+</form>									
+</table>
+
+    
+
+
+
+</font>
+</BODY>
+</HTML>
